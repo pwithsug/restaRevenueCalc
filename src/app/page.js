2affe41126710c1d0c14b7abaae4c4c42aa44bc2
pwithsug <ravine0226+4@gmail.com>
2025-04-27 05:46:19 +0900

    set responsive

--- src/app/page.js
+++ src/app/page.js
@@ -12,16 +12,16 @@ export default function Pricing() {
     <div className="min-h-screen flex flex-col items-center bg-primary  text-dark">
       {/* Headline */}
       <div className="bg-no-repeat bg-center bg-cover w-full" style={{backgroundImage: "url('/images/111.png')"}}>
-        <h1 className=" text-5xl font-extrabold text-center mt-30 mb-12 tracking-tight">
+        <h1 className=" xl:text-5xl lg:text-4xl sm:text-3xl font-extrabold text-center mt-30 mb-12 tracking-tight">
           THE RESTAURANT PROFIT CALCULATOR
         </h1>
-        <p className=" text-4xl  text-center mb-12">
+        <p className=" xl:text-4xl lg:text-3xl sm:text-2xl  text-center mb-12">
           Plan it like a pro. Dream it like a chef.
-          <br className="hidden md:inline" />
+          <br className="hidden sm:inline" />
           Decide it like an investor.
         </p>
         {/* CTA */}
-        <div className="flex flex-row items-center justify-center gap-4 text-white text-2xl mb-20">
+        <div className="flex flex-row items-center justify-center gap-4 text-white  xl:text-2xl  md:text-xl sm:text-xl mb-20">
           <button
             onClick={handleClick}
             className="w-[250px] bg-[#a82d23] hover:bg-[#a12d1b]  font-semibold py-5 rounded-3xl transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 focus:ring-4 focus:ring-[#a82d23]/50"
@@ -37,7 +37,7 @@ export default function Pricing() {
       </div>
         <div className="container bg-secondary w-full  px-6 py-10 shadow-md grid grid-cols-3 gap-6">
           <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold mb-2 flex items-center gap-2">
+            <h2 className="xl:text-2xl sm:text-xl font-bold mb-2 flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -62,7 +62,7 @@ export default function Pricing() {
             </p>
           </div>
           <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold mb-2 flex items-center gap-2">
+            <h2 className="xl:text-2xl sm:text-xl font-bold mb-2 flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -82,7 +82,7 @@ export default function Pricing() {
             This tool empowers owners to visualize their business model by entering simple metrics and instantly receiving a detailed financial forecast.</p>
           </div>
           <div className="flex flex-col items-center">
-          <h2 className="text-2xl font-bold mb-2 flex items-center gap-2">
+          <h2 className="xl:text-2xl sm:text-xl font-bold mb-2 flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -102,26 +102,26 @@ export default function Pricing() {
           </div>
         </div>
       {/* Pricing Cards */}
-      <div className=" mx-auto  pt-20  flex flex-col items-center">
-        <h2 className="text-4xl font-bold mb-8 text-center">UPGRADE FOR MORE FEATURES</h2>
-        <p className="text-xl text-center mb-8">
+      <div className=" mx-auto  xl:pt-20 sm:pt-10  flex flex-col items-center">
+        <h2 className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg font-bold xl:mb-8 sm:mb-4 text-center">UPGRADE FOR MORE FEATURES</h2>
+        <p className="2xl:text-xl xl:text-lg lg:text-md md:text-sm sm:text-xs text-center xl:mb-8 sm:mb-4">
           Get the most out of your restaurant planning with our premium features.
         </p>
       </div>
-      <div className="container  py-8 px-28  ">
+      <div className="container  py-8 2xl:px-28 lg:px-8 md:px-40 sm:px-30 ">
         {/* Basic */}
         <div className="flex-1 bg-[#c66c51] rounded-xl py-10 px-8 flex flex-col items-center shadow-lg shadow-[#0c1425]/50  w-full mx-auto text-[#0c1425] transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
-          <h2 className="text-5xl font-bold mb-1">BASIC</h2>
-          <div className="text-5xl font-extrabold mb-1">$59</div>
-          <div className=" text-3xl mb-3">one-time</div>
-          <ul className="list-disc list-inside  text-xl mb-5 space-y-1 text-left ">
+        <h2 className="xl:text-5xl  lg:text-3xl sm:text-2xl font-bold mb-1">Basic</h2>
+          <div className="xl:text-5xl  lg:text-3xl sm:text-2xl font-extrabold mb-1">$59</div>
+          <div className="xl:text-3xl  lg:text-2xl sm:text-xl mb-3">one-time</div>
+          <ul className="list-disc list-inside  2xl:text-xl xl:text-lg  sm:text-lg mb-5 space-y-1 text-left ">
             <li>Unlimited use for one concept</li>
             <li>Save + edit anytime</li>
             <li>Basic menu builder</li>
             <li>Downloadable report</li>
             <li>Set custom profit margin</li>
           </ul>
-          <button className="rounded border-2 border-[#0c1425] mt-auto inline-block px-5  overflow-hidden text-2xl font-bold transition-transform rounded-full group hover:bg-[#0c1425] hover:text-[#f8faf5]">
+          <button className="rounded border-2 border-[#0c1425] mt-auto inline-block px-5  overflow-hidden xl:text-2xl  md:text-xl sm:text-lg font-bold transition-transform rounded-full group hover:bg-[#0c1425] hover:text-[#f8faf5]">
             <a href="/basic" before="LEARN MORE" className="relative py-2 transition-transform inline-block before:content-[attr(before)] before:py-2 before:absolute before:top-full group-hover:-translate-y-full">
               LEARN MORE
             </a>
@@ -129,10 +129,10 @@ export default function Pricing() {
         </div>
         {/* Premium */}
         <div className="flex-1 bg-[#f6e187] rounded-xl py-10 px-8 flex flex-col items-center shadow-lg shadow-[#0c1425]/50 min-w-[260px] w-full mx-auto text-[#0c1425] transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
-          <h2 className="text-5xl font-bold mb-2">PREMIUM</h2>
-          <div className="text-5xl font-extrabold mb-3">$120</div>
-          <div className=" text-3xl mb-5">/year</div>
-          <ul className="list-disc list-inside  text-xl mb-5 space-y-1 text-left ">
+        <h2 className="xl:text-5xl  md:text-3xl sm:text-2xl font-bold mb-1">Premium</h2>
+          <div className="xl:text-5xl  md:text-3xl sm:text-2xl font-extrabold mb-1">$120</div>
+          <div className="xl:text-3xl  md:text-2xl sm:text-xl mb-3">/year</div>
+          <ul className="list-disc list-inside  xl:text-xl  sm:text-lg mb-5 space-y-1 text-left ">
             <li>Everything in Basic</li>
             <li>Unlimited concepts</li>
             <li>Basic menu builder</li>
@@ -140,7 +140,7 @@ export default function Pricing() {
             <li>Square footage auto-seat calculation</li>
             <li>Scenario comparison</li>
           </ul>
-          <button className="rounded border-2 border-[#0c1425] mt-auto inline-block px-5  overflow-hidden text-2xl font-bold transition-transform rounded-full group hover:bg-[#0c1425] hover:text-[#f8faf5]">
+          <button className="rounded border-2 border-[#0c1425] mt-auto inline-block px-5  overflow-hidden xl:text-2xl  md:text-xl sm:text-lg font-bold transition-transform rounded-full group hover:bg-[#0c1425] hover:text-[#f8faf5]">
             <a href="/premium" before="LEARN MORE" className="relative py-2 transition-transform inline-block before:content-[attr(before)] before:py-2 before:absolute before:top-full group-hover:-translate-y-full">
               LEARN MORE
             </a>
@@ -148,10 +148,10 @@ export default function Pricing() {
         </div>
         {/* Elite */}
         <div className="flex-1 bg-[#1d4d3a] rounded-xl py-10 px-8 flex flex-col items-center shadow-lg shadow-[#0c1425]/50 min-w-[260px] w-full mx-auto text-[#f8faf5] transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
-          <h2 className="text-5xl font-bold mb-2">Elite</h2>
-          <div className="text-5xl font-extrabold mb-3">$299</div>
-          <div className=" text-3xl mb-5">/year</div>
-          <ul className="list-disc list-inside  text-xl mb-5 space-y-1 text-left ">
+          <h2 className="xl:text-5xl  md:text-3xl sm:text-2xl font-bold mb-1">Elite</h2>
+          <div className="xl:text-5xl  md:text-3xl sm:text-2xl font-extrabold mb-1">$299</div>
+          <div className="xl:text-3xl  md:text-2xl sm:text-xl mb-3">/year</div>
+          <ul className="list-disc list-inside  2xl:text-xl xl:text-lg  sm:text-lg mb-5 space-y-1 text-left ">
             <li>All PREMIUM features</li>
             <li>Real vs. projected tracking inputs</li>
             <li>Staff training planner</li>
@@ -163,7 +163,7 @@ export default function Pricing() {
             <li>Priority support</li>
           </ul>
           
-          <button className="rounded border-2 border-right mt-auto inline-block px-5  overflow-hidden text-2xl font-bold transition-transform rounded-full group hover:bg-[#0c1425] hover:text-[#f8faf5]">
+          <button className="rounded border-2 border-[#0c1425] mt-auto inline-block px-5  overflow-hidden xl:text-2xl  md:text-xl sm:text-lg font-bold transition-transform rounded-full group hover:bg-[#0c1425] hover:text-[#f8faf5]">
             <a href="/elite" before="LEARN MORE" className="relative py-2 transition-transform inline-block before:content-[attr(before)] before:py-2 before:absolute before:top-full group-hover:-translate-y-full">
               LEARN MORE
             </a>
@@ -171,17 +171,17 @@ export default function Pricing() {
         </div>
       </div>
       {/* Profile */}
-      <div className="flex flex-col md:flex-row items-center gap-4 md:gap-8 mb-6">
+      <div className="container items-center gap-4 md:gap-8 mb-6">
         <img
           src="https://randomuser.me/api/portraits/men/32.jpg"
           alt="Elvis Restaino"
           className="w-70 h-70 rounded-full object-cover grayscale border-4 border-[#e3f4fa]"
         />
         <div className="text-center md:text-left">
-          <div className="text-dark text-4xl font-bold">
+          <div className="text-dark xl:text-4xl md:text-3xl sm:text-2xl font-bold">
             Elvis Restaino
           </div>
-          <div className="text-dark/80 text-3xl font-bold">
+          <div className="text-dark/80 xl:text-3xl md:text-2xl sm:text-xl font-bold">
             The best Restaurant Revenue Pro,
             <br />
             30+ years experience
@@ -189,11 +189,11 @@ export default function Pricing() {
         </div>
       </div>
       {/* Login link */}
-      <div className="text-center text-dark text-2xl font-bold mb-2">
+      <div className="text-center text-dark xl:text-3xl md:text-2xl sm:text-xl font-bold mb-2">
         Already a member?{" "}
         <a
           href="#"
-          className=" text-3xl underline font-bold hover:text-[#b23b2a]"
+          className=" xl:text-3xl md:text-2xl sm:text-xl underline font-bold hover:text-[#b23b2a]"
         >
           Log in
         </a>
